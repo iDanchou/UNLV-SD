3ee92445ce1b8afddd4ee73c3558e2e46ff861c1
Rasheen Kirkland <kirkland.rasheen@gmail.com>
2024-02-27 18:54:24 -0800

    progress after restart

---
+++ JS-Milestone-Project/milestone-project.js
@@ -0,0 +1,35 @@
+const buttonOptions = document.getElementById("buttons");
+const questionContainer = document.getElementById("question-container");
+
+let gryf = 0
+let huff = 0
+let raven = 0
+let slyth = 0
+
+var buttonA = document.getElementById('buttonA');
+var buttonB = document.getElementById('buttonB');
+var buttonC = document.getElementById('buttonC');
+var buttonD = document.getElementById('buttonD');
+
+
+buttonA.addEventListener('click', function() {
+  // alert('You clicked button A');
+  // You can add your logic here for button A
+});
+
+buttonB.addEventListener('click', function() {
+  // alert('You clicked button B');
+  // You can add your logic here for button B
+});
+
+buttonC.addEventListener('click', function() {
+  // alert('You clicked button C');
+  // You can add your logic here for button C
+});
+
+buttonD.addEventListener('click', function() {
+  // alert('You clicked button D');
+  // You can add your logic here for button D
+});
+
+buttonOptions.classList.add("hide");
